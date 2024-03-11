e9a1ea6b5426fecc55dccec3055cf33a27e74cd6
ceas <edu.alves1327@gmail.com>
2024-03-10 23:05:29 -0300

    Criando arquivo docker

---
+++ src/main/java/com/easyapp/adm/proeditor/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.easyapp.adm.proeditor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+       /* http.authorizeRequests()
+                .antMatchers("/**").permitAll() // Permitir acesso a todos os endpoints
+                .and().csrf().disable();*/
+    }
+}
